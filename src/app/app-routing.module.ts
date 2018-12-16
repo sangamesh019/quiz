0271589c4cfd38d55f28012c69cde0d20fd9c382
sangamesh019 <sangamhiremath@live.com>
2018-12-16 18:31:57 +0530

    updated the component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { LoginRegComponent } from './component/login-reg/login-reg.component';
+import { ContactUsComponent } from './component/contact-us/contact-us.component';
+import { CourcesComponent } from './component/cources/cources.component';
+import { FeedbackComponent } from './component/feedback/feedback.component';
+import { MaterialComponent } from './component/material/material.component';
+import { QuizComponent } from './component/quiz/quiz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '**', 
+  component:  HomeComponent
+  },
+  { 
+    path: 'login', 
+    component:  LoginRegComponent
+  },
+  { 
+    path: 'home', 
+    component:  
+    ContactUsComponent},
+  { 
+    path: 'home', 
+    component:  CourcesComponent
+  },
+  { 
+    path: 'home', 
+    component:  FeedbackComponent
+  },
+  { 
+    path: 'home', 
+    component:  MaterialComponent
+  },
+  { 
+    path: 'home', 
+    component:  QuizComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { QuizComponent } from './component/quiz/quiz.component';
+import { LoginRegComponent } from './component/login-reg/login-reg.component';
+import { ContactUsComponent } from './component/contact-us/contact-us.component';
+import { FeedbackComponent } from './component/feedback/feedback.component';
+import { MaterialComponent } from './component/material/material.component';
+import { CourcesComponent } from './component/cources/cources.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuizComponent,
+    LoginRegComponent,
+    ContactUsComponent,
+    FeedbackComponent,
+    MaterialComponent,
+    CourcesComponent
   ],
   imports: [
     BrowserModule,
